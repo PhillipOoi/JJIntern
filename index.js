ba64264f3f0653beb65680a957ff0e66ed1b346d
PhillipOoi <55256006+PhillipOoi@users.noreply.github.com>
2020-03-23 13:04:46 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+//removessssgt
+const init = async () => {
+
+    const server = Hapi.server({
+        port: 8000,
+        host: 'localhost'
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+
+            return 'Hello World!!@@@@@S';
+        }
+    });
+
+    server.route({
+        method: 'POST',
+        path: '/hello',
+        handler: function (request, h) {
+    
+            const payload = request.payload;
+    
+            return `Welcome ${payload.username}!!`;
+        }
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
